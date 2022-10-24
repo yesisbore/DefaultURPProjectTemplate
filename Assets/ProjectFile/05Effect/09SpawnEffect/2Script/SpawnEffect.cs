6d120449b56de523f27c8e7a669ef2bb8cca779f
Hong2 <60872762+yesisbore@users.noreply.github.com>
2022-10-25 02:05:25 +0900

    Add Character Spawn Effect
    Add Character Spawn Effect

---
+++ Assets/ProjectFile/05Effect/09SpawnEffect/2Script/SpawnEffect.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityCore.PlayerControl;
+using UnityEngine;
+
+namespace Effect
+{
+    public class SpawnEffect : MonoBehaviour
+    {
+   
+        #region Variables
+ 
+        public bool DebugMode = false;
+ 
+        // Public Variables
+    
+        // Private Variables
+        [SerializeField] private AnimationCurve _spawnCurve;
+        [SerializeField] private float _spawnTime = 3.0f;
+        [SerializeField] private Transform _spawnTransform;
+        [SerializeField] private float _enterPositionMultiple = 10.0f;
+        
+        private static readonly int SpawnProgress = Shader.PropertyToID("_SpawnProgress");
+        private Renderer _renderer;
+        private Coroutine _spawnCoroutine;
+        private bool _isSpawned = false;
+        private float _multipleTime;
+        private Vector3 _enterPosition;
+        
+        #endregion Variables
+
+        #region Unity Methods
+
+        private void Start() { Initialize();} // End of Unity - Start
+
+        //private void Update(){} // End of Unity - Update
+
+        #endregion Unity Methods
+
+        #region Public Methods
+
+        #endregion Public Methods
+    
+        #region Private Methods
+
+        private void Initialize()
+        {
+          GetComponents();
+          SubscribeInputEvent();
+          PlaceObjectInitPosition();
+          
+          _renderer.material.SetFloat(SpawnProgress, 1.0f);
+        } // End of Initialize
+
+        private void GetComponents()
+        {
+            _renderer = GetComponentInChildren<Renderer>();
+        } // End of GetComponents
+        private void SubscribeInputEvent()
+         {
+             ControllerInputs.Instance.OnSpaceBarPressed.AddListener(SpawnProcess);
+         } // End of SubscribeInputEvent
+        private void PlaceObjectInitPosition()
+        {
+            _enterPosition = _spawnTransform.position + Vector3.up * _enterPositionMultiple;
+            transform.position = _enterPosition;
+        } // End of PlaceObjectInitPosition
+        
+         private void SpawnProcess()
+         {
+             Log("Start Spawn Process");
+
+             var targetPosition = _isSpawned ? _enterPosition :_spawnTransform.position;
+             var targetValue = _isSpawned ? 1.0f : 0.0f;
+             _isSpawned = !_isSpawned;
+             
+             if(_spawnCoroutine != null) StopCoroutine(_spawnCoroutine);
+
+             _spawnCoroutine = StartCoroutine(Coroutine_Spawn(targetPosition,targetValue));
+         } // End of SpawnProcess
+
+         private IEnumerator Coroutine_Spawn(Vector3 targetPosition,float targetValue)
+         {
+             var startPosition = transform.position;
+             var startValue = _renderer.material.GetFloat(SpawnProgress);
+             var lerp = 0.0f;
+
+             _multipleTime = 1.0f / _spawnTime;
+             
+             while (lerp < 1)
+             {
+                 transform.position = Vector3.Lerp(startPosition, targetPosition, _spawnCurve.Evaluate(lerp));
+                 _renderer.material.SetFloat(SpawnProgress, Mathf.Lerp(startValue,targetValue,_spawnCurve.Evaluate(lerp)));
+                 lerp += Time.deltaTime * _multipleTime;
+                 yield return null;
+             }
+             _renderer.material.SetFloat(SpawnProgress, targetValue);
+             Log("End Spawn Process");
+         } // End of Coroutine_Spawn
+        #endregion Private Methods
+
+        #region Debug
+
+        private void Log(string msg)
+        {
+            if(!DebugMode) return;
+        
+            Logger.Log<SpawnEffect>( msg);
+        }
+        
+        private void LogWarning(string msg)
+        {
+            if(!DebugMode) return;
+                    
+            Logger.LogWarning<SpawnEffect>(msg);
+        }
+
+        #endregion Debug
+    }
+}
+
