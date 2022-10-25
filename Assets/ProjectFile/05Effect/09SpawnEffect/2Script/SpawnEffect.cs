5097bf0e06cc1d90b0bce9e5b0cb1c06ff3f9528
Hong2 <60872762+yesisbore@users.noreply.github.com>
2022-10-26 02:52:31 +0900

    Add Landing Animation
    Add Landing Animation

--- Assets/ProjectFile/05Effect/09SpawnEffect/2Script/SpawnEffect.cs
+++ Assets/ProjectFile/05Effect/09SpawnEffect/2Script/SpawnEffect.cs
@@ -20,13 +20,18 @@ namespace Effect
         [SerializeField] private Transform _spawnTransform;
         [SerializeField] private float _enterPositionMultiple = 10.0f;
         
+        // Hash
         private static readonly int SpawnProgress = Shader.PropertyToID("_SpawnProgress");
+        private static readonly int Landing = Animator.StringToHash("Landing");
+
         private Renderer _renderer;
+        private Animator _animator;
         private Coroutine _spawnCoroutine;
         private bool _isSpawned = false;
+        private bool _isLanding = false;
         private float _multipleTime;
         private Vector3 _enterPosition;
-        
+
         #endregion Variables
 
         #region Unity Methods
@@ -55,6 +60,7 @@ namespace Effect
         private void GetComponents()
         {
             _renderer = GetComponentInChildren<Renderer>();
+            _animator = GetComponent<Animator>();
         } // End of GetComponents
         private void SubscribeInputEvent()
          {
@@ -73,6 +79,7 @@ namespace Effect
              var targetPosition = _isSpawned ? _enterPosition :_spawnTransform.position;
              var targetValue = _isSpawned ? 1.0f : 0.0f;
              _isSpawned = !_isSpawned;
+             _isLanding = false;
              
              if(_spawnCoroutine != null) StopCoroutine(_spawnCoroutine);
 
@@ -86,19 +93,36 @@ namespace Effect
              var lerp = 0.0f;
 
              _multipleTime = 1.0f / _spawnTime;
-             
+
              while (lerp < 1)
              {
                  transform.position = Vector3.Lerp(startPosition, targetPosition, _spawnCurve.Evaluate(lerp));
                  _renderer.material.SetFloat(SpawnProgress, Mathf.Lerp(startValue,targetValue,_spawnCurve.Evaluate(lerp)));
                  lerp += Time.deltaTime * _multipleTime;
+                 if (_isSpawned && lerp >=0.65f)
+                 {
+                     AnimateLanding();
+                 }
                  yield return null;
              }
              _renderer.material.SetFloat(SpawnProgress, targetValue);
              Log("End Spawn Process");
          } // End of Coroutine_Spawn
+
+
         #endregion Private Methods
 
+        #region Animation
+
+        private void AnimateLanding()
+        {
+            if(_isLanding) return;
+            
+            _isLanding = true;
+            _animator.SetTrigger(Landing);
+        } // End of Landing
+
+        #endregion
         #region Debug
 
         private void Log(string msg)
